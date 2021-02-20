8077312dadd4fa3d78e2bd36241eace14cb62175
harshhgupta <harshhgupta16@gmail.com>
2021-02-20 22:32:51 +0530

    increase qunatity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,6 +10,15 @@ class CartItem extends Component {
             qty: 1,
             img : ''
         }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+
+    // increaseQuantity() {
+    //     console.log(this.state);
+    // }
+
+    increaseQuantity = () => {
+        console.log('Hello',this.state)
     }
 
     render(){
@@ -22,11 +31,21 @@ class CartItem extends Component {
                  <div className="right-block">
                     <div style={ {fontSize: 25} }>{title}</div>
                     <div style={ {color: '#777'} }>Rs {price}</div>
-                    <div style={ {color: '#777'} }>Qty: {qty}</div>
+                    <div style={ {color: '#777'} }>Qty: { qty}</div>
                     <div className="cart-item-actions">
-                        <img alt="increase" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/1828/1828926.svg?token=exp=1613830116~hmac=ac9edf273f849ffb04d5fe0dcbe302b2" />
-                        <img alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/1828/1828906.svg?token=exp=1613830073~hmac=71604b9ba1c45e3eea14a54106e8b53e" />
-                        <img alt="delete" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/3096/3096673.svg?token=exp=1613830169~hmac=e6ae55609e6908e1d052428e6c3ca79d" />
+                        <img 
+                            alt="increase" 
+                            className="action-icons" 
+                            src="https://www.flaticon.com/svg/vstatic/svg/1828/1828926.svg?token=exp=1613830116~hmac=ac9edf273f849ffb04d5fe0dcbe302b2" 
+                            onClick={this.increaseQuantity}/>
+                        <img 
+                            alt="decrease" 
+                            className="action-icons" 
+                            src="https://www.flaticon.com/svg/vstatic/svg/1828/1828906.svg?token=exp=1613830073~hmac=71604b9ba1c45e3eea14a54106e8b53e" />
+                        <img 
+                            alt="delete" 
+                            className="action-icons" 
+                            src="https://www.flaticon.com/svg/vstatic/svg/3096/3096673.svg?token=exp=1613830169~hmac=e6ae55609e6908e1d052428e6c3ca79d" />
                     </div>
                  </div>
             </div>
