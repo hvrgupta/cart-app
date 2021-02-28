91ad88ae24a25d5b647ec1cf16aa63876061a7e7
harshhgupta <harshhgupta16@gmail.com>
2021-02-28 18:37:08 +0530

    Added Firebase

--- src/App.js
+++ src/App.js
@@ -1,35 +1,77 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import firebase from 'firebase';
 
 class App extends React.Component {
  
   constructor() {
     super();
     this.state = {
-            products: [{
-                price: 10000,
-                title: 'Phone',
-                qty: 1,
-                img : 'https://images.unsplash.com/photo-1580910051074-3eb694886505?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8cGhvbmV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-                id:1
-            },
-            {
-                price: 1000,
-                title: 'Watch',
-                qty: 1,
-                img : 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0Y2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-                id:2
-            },
-            {
-                price: 100,
-                title: 'Pen',
-                qty: 1,
-                img : 'https://images.unsplash.com/photo-1583485088034-697b5bc54ccd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8cGVufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-                id:3
-            }]
+            products: [],
+            loading: true
+            // products: [{
+            //     price: 10000,
+            //     title: 'Phone',
+            //     qty: 1,
+            //     img : 'https://images.unsplash.com/photo-1580910051074-3eb694886505?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8cGhvbmV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+            //     id:1
+            // },
+            // {
+            //     price: 1000,
+            //     title: 'Watch',
+            //     qty: 1,
+            //     img : 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0Y2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+            //     id:2
+            // },
+            // {
+            //     price: 100,
+            //     title: 'Pen',
+            //     qty: 1,
+            //     img : 'https://images.unsplash.com/photo-1583485088034-697b5bc54ccd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8cGVufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+            //     id:3
+            // }]
     }
     // this.increaseQuantity = this.increaseQuantity.bind(this);
+}
+
+componentDidMount() {
+  //firebase
+  // .firestore()
+  // .collection('products')
+  // .get()
+  // .then((snapshot) => {
+  //   console.log(snapshot);
+
+  //   const products = snapshot.docs.map((doc) => {
+  //     const data = doc.data();
+  //     data['id'] = doc.id;
+  //     return data;
+  //   })
+
+  //   this.setState({
+  //     products,
+  //     loading: false
+  //   })
+  // })
+
+ firebase
+  .firestore()
+  .collection('products')
+  .onSnapshot((snapshot) => {
+    console.log(snapshot);
+
+    const products = snapshot.docs.map((doc) => {
+      const data = doc.data();
+      data['id'] = doc.id;
+      return data;
+    })
+
+    this.setState({
+      products,
+      loading: false
+    })
+  })
 }
 
 handleIncreaseQuantity = (product) => {
@@ -99,7 +141,7 @@ getCartTotal = () => {
 }
 
   render(){
-    const products = this.state.products;
+    const { products,loading } = this.state;
     return (
       <div>
         <Navbar count={this.getCartCount()}/>
@@ -109,6 +151,7 @@ getCartTotal = () => {
             onDeleteProduct={this.handleDeleteProduct}
             products={products}
         />
+        {loading && <h1>Loading...</h1>}
         <div className="total">TOTAL : {this.getCartTotal()}</div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClG1ItI2HrpMdNhdMh_f4FYNLe7vIOSOg",
+  authDomain: "cart-app-26878.firebaseapp.com",
+  projectId: "cart-app-26878",
+  storageBucket: "cart-app-26878.appspot.com",
+  messagingSenderId: "409875072638",
+  appId: "1:409875072638:web:892d2305d8eca09745fa2f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
